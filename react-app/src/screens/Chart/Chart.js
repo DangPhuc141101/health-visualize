558437946a62352667c1ed683955ec0f9bb650dc
trantuyen082001@gmail.com <trantuyen082001@gmail.com>
2022-05-10 19:49:39 +0700

    update drop and drag

--- react-app/src/screens/Chart/Chart.js
+++ react-app/src/screens/Chart/Chart.js
@@ -1,19 +1,99 @@
-import React from 'react';
-import { ListGroup, Dropdown, InputGroup, FormControl} from 'react-bootstrap';
-import Draggable from 'react-draggable';
+import React, { useState } from 'react';
+import { Dropdown, InputGroup, ListGroup } from 'react-bootstrap';
 import { AiOutlineDotChart } from 'react-icons/ai';
 import { BsBarChart } from 'react-icons/bs';
 import { FcLineChart, FcPieChart } from 'react-icons/fc';
 import { IoMdArrowDropdownCircle } from 'react-icons/io';
 import { MdOutlineStackedBarChart } from 'react-icons/md';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { RiBarChartGroupedFill, RiBarChartHorizontalFill, RiBarChartHorizontalLine } from 'react-icons/ri';
-import DropFile from '../DropFile/DropFile';
 import './chart.css';
 
 const Chart = (props) => {
-    console.log(props.data);
-    console.log(props.columns);
+    const [xAxis, setXAxis] = useState([]);
+    const [yAxis, setYAxis] = useState([]);
+    const [legend, setLegend] = useState([]);
+    
+    
+    const handlerXAxis = (id) => {
+      if(xAxis.includes(id) === true) {
+        return;
+      }
+      setXAxis(pre => [...pre, id])
+    }
+
+    const handlerYAxis = (id) => {
+      if(yAxis.includes(id) === true) {
+        return;
+      }
+      setYAxis(pre => [...pre, id])
+    }
+
+    const handlerLegend = (id) => {
+      if(legend.includes(id) === true) {
+        return;
+      }
+      setLegend(pre => [...pre, id])
+    }
+
+    const dragstart_handler = (e, column) => {
+      e.dataTransfer.setData("text", column)
+      e.effectAllowed = 'copyMove';
+    }
+
+    const dragover_handler = (e) => {
+      e.preventDefault();
+    }
+
+    const drop_handler_xAxis = (e) => {
+      e.preventDefault();
+      e.currentTarget.style.background = "lightblue";
+      const id = e.dataTransfer.getData("text");
+      handlerXAxis(id);
+      if (id == "src_move" && e.target.id == "dest_move"){
+        e.target.appendChild(document.getElementById(id));
+        if (id == "src_copy" && e.target.id == "dest_copy") {
+          var nodeCopy = document.getElementById(id).cloneNode(true);
+          nodeCopy.id = "newId";
+          e.target.appendChild(nodeCopy);
+      }
+    }
+  }
+    const drop_handler_YAxis = (e) => {
+      e.preventDefault();
+      e.currentTarget.style.background = "lightblue";
+      const id = e.dataTransfer.getData("text");
+      handlerYAxis(id);
+      if (id == "src_move" && e.target.id == "dest_move"){
+        e.target.appendChild(document.getElementById(id));
+        if (id == "src_copy" && e.target.id == "dest_copy") {
+          var nodeCopy = document.getElementById(id).cloneNode(true);
+          nodeCopy.id = "newId";
+          e.target.appendChild(nodeCopy);
+      }
+    }
+  }
+
+    const drop_handler_legend = (e) => {
+      e.preventDefault();
+      e.currentTarget.style.background = "lightblue";
+      const id = e.dataTransfer.getData("text");
+      handlerLegend(id);
+      if (id == "src_move" && e.target.id == "dest_move"){
+        e.target.appendChild(document.getElementById(id));
+        if (id == "src_copy" && e.target.id == "dest_copy") {
+          var nodeCopy = document.getElementById(id).cloneNode(true);
+          nodeCopy.id = "newId";
+          e.target.appendChild(nodeCopy);
+      }
+    }
+  }
+
+  const dragend_handler = (e) => {
+    e.target.style.border = "solid black";
+    e.dataTransfer.clearData();
+  }
+
+
   return (
     <>
         <div className='chart_container'>
@@ -28,13 +108,22 @@ const Chart = (props) => {
                        <span>Name of datasets</span>
                     </div>
                     <div className='dataset_list'>
-                        {props.columns.map((e) => (
-                          <Draggable>
-                              <ListGroup>
-                                {/* {console.log(typeof e)} */}
-                                    <ListGroup.Item>{e}</ListGroup.Item>
-                              </ListGroup>
-                          </Draggable>
+                        {props.columns.map((column) => (
+                          // <Draggable>
+                          //     <ListGroup>
+                          //       {/* {console.log(typeof e)} */}
+                          //           <ListGroup.Item>{e}</ListGroup.Item>
+                          //     </ListGroup>
+                          // </Draggable>
+                          <div 
+                            draggable='true' 
+                            id='src_copy' 
+                            onDragStart={(e) => dragstart_handler(e,column)}
+                            onDragEnd={(e) => dragend_handler(e)}>
+                            <ListGroup>
+                              <ListGroup.Item>{column}</ListGroup.Item>
+                            </ListGroup>
+                          </div>
                         ))}
                     </div>
                   </div>
@@ -102,13 +191,61 @@ const Chart = (props) => {
                       <div className='axis_container'>
                             {/* === X === */}
                             <InputGroup.Text id="basic-addon1">X-Axis</InputGroup.Text>
-                            <DropFile/>
+                            <div 
+                              id='dest_copy' 
+                              onDrop={(e) => drop_handler_xAxis(e)} 
+                              onDragOver={(e) => dragover_handler(e)}
+                              className='x_Axis'
+                            >
+                              {xAxis != 0 ? 
+                                      <div>
+                                      {xAxis.map((e) =>
+                                        <ul>
+                                          <li>{e}</li>
+                                        </ul>
+                                      )}
+                                      </div> 
+                                      : 
+                                      <p>Drop here</p>}
+                            </div>
                             {/* === Y === */}
                             <InputGroup.Text id="basic-addon2">Y-Axis</InputGroup.Text>
-                            <DropFile/>
+                            <div 
+                              id='dest_copy' 
+                              onDrop={(e) => drop_handler_YAxis(e)} 
+                              onDragOver={(e) => dragover_handler(e)}
+                              className='y_Axis'
+                            >
+                              {yAxis != 0 ? 
+                                      <div>
+                                      {yAxis.map((e) =>
+                                        <ul>
+                                          <li>{e}</li>
+                                        </ul>
+                                      )}
+                                      </div> 
+                                      : 
+                                      <p>Drop here</p>}
+                            </div>
                             {/* === Legend === */}
                             <InputGroup.Text id="basic-addon3">Legend</InputGroup.Text>
-                            <DropFile/>
+                            <div 
+                              id='dest_copy' 
+                              onDrop={(e) => drop_handler_legend(e)} 
+                              onDragOver={(e) => dragover_handler(e)}
+                              className='legend'
+                            >
+                              {legend != 0 ? 
+                                      <div>
+                                      {legend.map((e) =>
+                                        <ul>
+                                          <li>{e}</li>
+                                        </ul>
+                                      )}
+                                      </div> 
+                                      : 
+                                      <p>Drop here</p>}
+                            </div>
                       </div>
                   </div>
                 </div>
