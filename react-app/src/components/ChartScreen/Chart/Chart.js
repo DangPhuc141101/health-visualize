6cab97401f4625d7a074cb8816534b8c017a8a2e
trantuyen082001@gmail.com <trantuyen082001@gmail.com>
2022-07-27 14:29:31 +0700

    check

--- react-app/src/components/ChartScreen/Chart/Chart.js
+++ react-app/src/components/ChartScreen/Chart/Chart.js
@@ -1,25 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './chart.css';
-import ChartItems from '../ChartItems/ChartItems';
-import DataColumns from '../dataColumns/DataColumns';
+import ChartItems from "../ChartItems/ChartItems";
+import DataColumns from "../dataColumns/DataColumns";
+import "./chart.css";
 
-import Legend from '../ChartField/legend/Legend';
-import Size from '../ChartField/size/Size';
-import SmallMultiples from '../ChartField/smallMultiples/SmallMultiples';
-import Value from '../ChartField/value/Value';
-import Xaxis from '../ChartField/xAxis/Xaxis';
-import Yaxis from '../ChartField/yAxis/Yaxis';
-import SecondaryY_Axis from '../ChartField/secondaryY_Axis/SecondaryY_Axis';
+import Legend from "../ChartField/legend/Legend";
+import SecondaryY_Axis from "../ChartField/secondaryY_Axis/SecondaryY_Axis";
+import Size from "../ChartField/size/Size";
+import SmallMultiples from "../ChartField/smallMultiples/SmallMultiples";
+import Value from "../ChartField/value/Value";
+import Xaxis from "../ChartField/xAxis/Xaxis";
+import Yaxis from "../ChartField/yAxis/Yaxis";
 
-import AreaChart from '../../Charts/AreaChart';
-import ColumnChart from '../../Charts/ColumnChart';
-import BarChart from '../../Charts/BarChart';
-import PieChart from '../../Charts/PieChart';
-import LineChart from '../../Charts/LineChart';
-import MultipleChart from '../../Charts/MultipleChart';
-import ResizeDrag from '../../Charts/ResizeDrag';
-
+import AreaChart from "../../Charts/AreaChart";
+import BarChart from "../../Charts/BarChart";
+import LineChart from "../../Charts/LineChart";
+import MultipleChart from "../../Charts/MultipleChart";
+import ResizeDrag from "../../Charts/ResizeDrag";
+import { AiOutlineDoubleRight } from "react-icons/ai";
 
 const Chart = (props) => {
   const { listObjData } = props;
@@ -30,8 +28,11 @@ const Chart = (props) => {
   const [values, setValues] = useState([]);
   const [sizes, setSizes] = useState([]);
   const [secondaryY_Axis, setSecondaryY_Axis] = useState([]);
-  const [typeChart, setTypeChart] = useState('');
+  const [typeChart, setTypeChart] = useState("");
 
+  const [isActive, setIsActive] = useState(true);
+
+  const [activeNameChart, setActiveNameChart] = useState(true);
 
   // ====== Get Chart from Chart Items ========
   const handlerTypeChart = (typeChart) => {
@@ -42,15 +43,15 @@ const Chart = (props) => {
     setSmallMultiples([]);
     setValues([]);
     setSizes([]);
-  }
+  };
 
   //  ====== X-Axis =====
   const handlerXAxis = (id) => {
     if (xAxis.includes(id) === true) {
       return;
     }
-    setXAxis(pre => [...pre, id])
-  }
+    setXAxis((pre) => [...pre, id]);
+  };
 
   // ===== support drop handler ====
   const supportHandler = (e, id) => {
@@ -62,173 +63,177 @@ const Chart = (props) => {
         e.target.appendChild(nodeCopy);
       }
     }
-  }
+  };
   const drop_handler_xAxis = (e) => {
     e.preventDefault();
     e.currentTarget.style.background = "lightblue";
     const id = e.dataTransfer.getData("text");
-    supportHandler(e,id)
+    supportHandler(e, id);
     handlerXAxis(id);
-  }
+  };
 
   const handleDeletedXAxis = (e) => {
-    const data = xAxis.filter((item) => item !== e)
+    const data = xAxis.filter((item) => item !== e);
     setXAxis(data);
-  }
+  };
 
   // ====== Y-Axis =====
   const handlerYAxis = (id) => {
     if (yAxis.includes(id) === true) {
       return;
     }
-    setYAxis(pre => [...pre, id])
-  }
+    setYAxis((pre) => [...pre, id]);
+  };
 
   const drop_handler_YAxis = (e) => {
     e.preventDefault();
     e.currentTarget.style.background = "lightblue";
     const id = e.dataTransfer.getData("text");
-    
-    supportHandler(e,id);
+
+    supportHandler(e, id);
     handlerYAxis(id);
-  }
+  };
 
   const handleDeletedYAxis = (e) => {
-    const data = yAxis.filter((item) => item !== e)
+    const data = yAxis.filter((item) => item !== e);
     setYAxis(data);
-  }
+  };
 
   // ====== Legend =====
   const handlerLegend = (id) => {
     if (legend.includes(id) === true) {
       return;
     }
-    setLegend(pre => [...pre, id])
-  }
+    setLegend((pre) => [...pre, id]);
+  };
 
   const drop_handler_legend = (e) => {
     e.preventDefault();
     e.currentTarget.style.background = "lightblue";
     const id = e.dataTransfer.getData("text");
-    
-    supportHandler(e,id)
+
+    supportHandler(e, id);
     handlerLegend(id);
-  }
+  };
 
   const handleDeletedLegend = (e) => {
-    const data = legend.filter((item) => item !== e)
+    const data = legend.filter((item) => item !== e);
     setLegend(data);
-  }
+  };
 
   //  ====== Small Multiples =====
   const handlerSmallMultiples = (id) => {
     if (smallMultiples.includes(id) === true) {
       return;
     }
-    setSmallMultiples(pre => [...pre, id])
-  }
+    setSmallMultiples((pre) => [...pre, id]);
+  };
 
   const drop_handler_smallMultiples = (e) => {
     e.preventDefault();
     e.currentTarget.style.background = "lightblue";
     const id = e.dataTransfer.getData("text");
-    supportHandler(e,id);
+    supportHandler(e, id);
     handlerSmallMultiples(id);
-  }
+  };
 
   const handleDeletedSmallMultiples = (e) => {
-    const data = smallMultiples.filter((item) => item !== e)
+    const data = smallMultiples.filter((item) => item !== e);
     setSmallMultiples(data);
-  }
+  };
 
   //  ====== Value =====
   const handlerValues = (id) => {
     if (values.includes(id) === true) {
       return;
     }
-    setValues(pre => [...pre, id])
-  }
+    setValues((pre) => [...pre, id]);
+  };
 
   const drop_handler_Values = (e) => {
     e.preventDefault();
     e.currentTarget.style.background = "lightblue";
     const id = e.dataTransfer.getData("text");
-    supportHandler(e,id)
+    supportHandler(e, id);
     handlerValues(id);
-  }
+  };
 
   const handleDeletedValue = (e) => {
-    const data = values.filter((item) => item !== e)
+    const data = values.filter((item) => item !== e);
     setValues(data);
-  }
+  };
 
   //  ====== Size =====
   const handlerSizes = (id) => {
     if (sizes.includes(id) === true) {
       return;
     }
-    setSizes(pre => [...pre, id])
-  }
+    setSizes((pre) => [...pre, id]);
+  };
 
   const drop_handler_sizes = (e) => {
     e.preventDefault();
     e.currentTarget.style.background = "lightblue";
     const id = e.dataTransfer.getData("text");
-    
-    supportHandler(e,id)
+
+    supportHandler(e, id);
     handlerSizes(id);
-  }
+  };
 
   const handleDeletedSize = (e) => {
-    const data = sizes.filter((item) => item !== e)
+    const data = sizes.filter((item) => item !== e);
     setSizes(data);
-  }
+  };
 
   //  ====== Secondary Y Axis =====
   const handlerSecondaryY_Axis = (id) => {
     if (secondaryY_Axis.includes(id) === true) {
       return;
     }
-    setSecondaryY_Axis(pre => [...pre, id])
-  }
+    setSecondaryY_Axis((pre) => [...pre, id]);
+  };
 
   const drop_handler_SecondaryY_Axis = (e) => {
     e.preventDefault();
     e.currentTarget.style.background = "lightblue";
     const id = e.dataTransfer.getData("text");
-    
-    supportHandler(e,id);
+
+    supportHandler(e, id);
     handlerSecondaryY_Axis(id);
-  }
+  };
 
   const handleDeletedSecondaryY_Axis = (e) => {
-    const data = secondaryY_Axis.filter((item) => item !== e)
+    const data = secondaryY_Axis.filter((item) => item !== e);
     setSecondaryY_Axis(data);
-  }
+  };
 
   // ========= Drag - Drop =========
   const dragstart_handler = (e, column) => {
-    e.dataTransfer.setData("text", column)
-    e.effectAllowed = 'copyMove';
-  }
+    e.dataTransfer.setData("text", column);
+    e.effectAllowed = "copyMove";
+  };
 
   const dragover_handler = (e) => {
     e.preventDefault();
-  }
+  };
 
   const dragend_handler = (e) => {
     e.target.style.border = "solid gray";
     e.dataTransfer.clearData();
-  }
+  };
 
+  // =========== Handle UnDisplay ===========
+  const handleDisplayCompent = () => {
+    console.log("first check: ");
+  };
 
   return (
     <>
-      <div className='chart_container'>
+      <div className="chart_container">
         {/* ======== left ========= */}
-        <div className='chart_title'>
+        <div className="chart_title">
           {/* ======= Name of dataset and filter ====== */}
-           <div className='chart_columns'>
+          <div className="chart_columns">
             {/* Name of dataset */}
             <DataColumns
               columns={props.columns}
@@ -238,107 +243,158 @@ const Chart = (props) => {
           </div>
 
           {/* ======== Name of chart and Axis ========= */}
-         <div className='chart_names'>
-            <ChartItems getTypeChart={handlerTypeChart} />
+          <div className="chart_names">
+            <ChartItems
+              getTypeChart={handlerTypeChart}
+              getAction={handleDisplayCompent}
+            />
             {/* ===== Axis ====== */}
-            <div className='chart_axis'>
-              <div className='axis_container'>
+            <div className="chart_axis">
+              <div className="axis_container">
                 {/* === X === */}
-                {typeChart === 'bar' || typeChart === 'scatter' || typeChart === 'line' || typeChart === 'column' || typeChart === 'area' ?
+                {typeChart === "bar" ||
+                typeChart === "scatter" ||
+                typeChart === "line" ||
+                typeChart === "column" ||
+                typeChart === "area" ? (
                   <Xaxis
                     xAxis={xAxis}
                     drop_handler_xAxis={drop_handler_xAxis}
                     dragover_handler={dragover_handler}
                     handleDeletedXAxis={handleDeletedXAxis}
-                  /> : (null)
-                }
+                  />
+                ) : null}
                 {/* === Y === */}
-                {typeChart === 'bar' || typeChart === 'scatter' || typeChart === 'line' || typeChart === 'column' || typeChart === 'area' ?
+                {typeChart === "bar" ||
+                typeChart === "scatter" ||
+                typeChart === "line" ||
+                typeChart === "column" ||
+                typeChart === "area" ? (
                   <Yaxis
                     yAxis={yAxis}
                     drop_handler_YAxis={drop_handler_YAxis}
                     dragover_handler={dragover_handler}
                     handleDeletedYAxis={handleDeletedYAxis}
-                  /> : (null)
-                }
+                  />
+                ) : null}
                 {/* === Legend === */}
-                {typeChart === 'pie' || typeChart === 'bar' || typeChart === 'scatter' || typeChart === 'line' || typeChart === 'column' || typeChart === 'area' ?
+                {typeChart === "pie" ||
+                typeChart === "bar" ||
+                typeChart === "scatter" ||
+                typeChart === "line" ||
+                typeChart === "column" ||
+                typeChart === "area" ? (
                   <Legend
                     legend={legend}
                     drop_handler_legend={drop_handler_legend}
                     dragover_handler={dragover_handler}
                     handleDeletedLegend={handleDeletedLegend}
-                  /> : (null)
-                }
+                  />
+                ) : null}
                 {/* === SmallMultiples === */}
-                {typeChart === 'bar' || typeChart === 'line' || typeChart === 'column' || typeChart === 'area' ?
+                {typeChart === "bar" ||
+                typeChart === "line" ||
+                typeChart === "column" ||
+                typeChart === "area" ? (
                   <SmallMultiples
                     smallMultiples={smallMultiples}
                     drop_handler_smallMultiples={drop_handler_smallMultiples}
                     dragover_handler={dragover_handler}
                     handleDeletedSmallMultiples={handleDeletedSmallMultiples}
                   />
-                  : (null)}
+                ) : null}
                 {/* === Value === */}
-                {typeChart === 'pie' || typeChart === '' || typeChart === 'scatter' ?
+                {typeChart === "pie" ||
+                typeChart === "" ||
+                typeChart === "scatter" ? (
                   <Value
                     values={values}
                     drop_handler_Values={drop_handler_Values}
                     dragover_handler={dragover_handler}
                     handleDeletedValue={handleDeletedValue}
                   />
-                  : (null)
-                }
+                ) : null}
                 {/* === Size === */}
-                {typeChart === 'scatter' ?
+                {typeChart === "scatter" ? (
                   <Size
                     sizes={sizes}
                     drop_handler_sizes={drop_handler_sizes}
                     dragover_handler={dragover_handler}
                     handleDeletedSize={handleDeletedSize}
                   />
-                  : (null)
-                }
+                ) : null}
                 {/* === Secondary === */}
-                {typeChart === 'area' ?
+                {typeChart === "area" ? (
                   <SecondaryY_Axis
                     secondaryY_Axis={secondaryY_Axis}
                     drop_handler_SecondaryY_Axis={drop_handler_SecondaryY_Axis}
                     dragover_handler={dragover_handler}
                     handleDeletedSecondaryY_Axis={handleDeletedSecondaryY_Axis}
                   />
-                  : (null)
-                }
-
+                ) : null}
               </div>
             </div>
           </div>
         </div>
         {/* ====== right ========= */}
-        <div className='chart_draw'>
-          {console.log(listObjData)}          
-          {typeChart === "pie" && listObjData && values[0] && legend[0] ? <ResizeDrag value={values[0]} legend={legend[0]} data={props.listObjData}></ResizeDrag> : (<p>Helllllo</p>)}
-          {typeChart === "bar" && listObjData && xAxis[0] && yAxis.length > 0 ? <BarChart yAxis={yAxis} xAxis={xAxis[0]} data={props.listObjData} legend={legend[0]}></BarChart> : (null)}
-          {typeChart === "column" && listObjData && xAxis[0] && yAxis.length > 0  && smallMultiples.length > 0? <MultipleChart smallMultiple={smallMultiples[0]} yAxis={yAxis} xAxis={xAxis[0]} data={props.listObjData} legend={legend[0]}></MultipleChart> : (null)}
+        <div className="chart_draw">
+          {console.log(listObjData)}
+          {typeChart === "pie" && listObjData && values[0] && legend[0] ? (
+            <ResizeDrag
+              value={values[0]}
+              legend={legend[0]}
+              data={props.listObjData}
+            ></ResizeDrag>
+          ) : (
+            <p>Helllllo</p>
+          )}
+          {typeChart === "bar" &&
+          listObjData &&
+          xAxis[0] &&
+          yAxis.length > 0 ? (
+            <BarChart
+              yAxis={yAxis}
+              xAxis={xAxis[0]}
+              data={props.listObjData}
+              legend={legend[0]}
+            ></BarChart>
+          ) : null}
+          {typeChart === "column" &&
+          listObjData &&
+          xAxis[0] &&
+          yAxis.length > 0 &&
+          smallMultiples.length > 0 ? (
+            <MultipleChart
+              smallMultiple={smallMultiples[0]}
+              yAxis={yAxis}
+              xAxis={xAxis[0]}
+              data={props.listObjData}
+              legend={legend[0]}
+            ></MultipleChart>
+          ) : null}
           {/* {typeChart === "pie" && props.listObjData && xAxis && yAxis.length > 0 ? <PieChart value={yAxis} legend={xAxis} data={props.listObjData}></PieChart> : (null)} */}
-          {typeChart === "area" && props.listObjData && xAxis && yAxis.length > 0 ? <AreaChart yAxis={yAxis} xAxis={xAxis[0]} data={props.listObjData}></AreaChart> : (null)}
-          {typeChart === "line" &&
-            listObjData &&
-            xAxis &&
-            yAxis.length > 0 ? (
+          {typeChart === "area" &&
+          props.listObjData &&
+          xAxis &&
+          yAxis.length > 0 ? (
+            <AreaChart
+              yAxis={yAxis}
+              xAxis={xAxis[0]}
+              data={props.listObjData}
+            ></AreaChart>
+          ) : null}
+          {typeChart === "line" && listObjData && xAxis && yAxis.length > 0 ? (
             <LineChart
               yAxis={yAxis}
               xAxis={xAxis[0]}
               data={props.listObjData}
               legend={legend[0]}
             ></LineChart>
-          ) : (
-            (null)
-          )}
+          ) : null}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Chart;
--- react-app/src/components/ChartScreen/ChartItems/ChartItems.js
+++ react-app/src/components/ChartScreen/ChartItems/ChartItems.js
@@ -1,154 +1,165 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { AiOutlineDotChart } from 'react-icons/ai';
-import { BiTable } from 'react-icons/bi';
-import { BsBarChart } from 'react-icons/bs';
-import { FcLineChart, FcPieChart, FcAreaChart, FcDoughnutChart } from 'react-icons/fc';
-import { MdOutlineStackedBarChart } from 'react-icons/md';
-import { RiBarChartGroupedFill, RiBarChartHorizontalFill, RiBarChartHorizontalLine } from 'react-icons/ri';
-import './chartItems.css';
+import React, { useEffect, useRef, useState } from "react";
+import { AiOutlineDotChart, AiOutlineDoubleLeft } from "react-icons/ai";
+import { BiTable } from "react-icons/bi";
+import { BsBarChart } from "react-icons/bs";
+import {
+  FcLineChart,
+  FcPieChart,
+  FcAreaChart,
+  FcDoughnutChart,
+} from "react-icons/fc";
+import { MdOutlineStackedBarChart } from "react-icons/md";
+import {
+  RiBarChartGroupedFill,
+  RiBarChartHorizontalFill,
+  RiBarChartHorizontalLine,
+} from "react-icons/ri";
+import "./chartItems.css";
 
 const ChartItems = (props) => {
-    const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-    const chartMenu = useRef();
+  const chartMenu = useRef();
 
-    useEffect(() => {
-        let handler = (e) => {
-            if(!chartMenu.current.contains(e.target)) {
-                setIsActive(false)
-            }
-        }
-
-        document.addEventListener("mousedown", handler);
-
-        return () => {
-            document.removeEventListener("mousedown", handler);
-        }
-    })
-
-    const ToggleClass = () => {
-        setIsActive(!isActive);
+  useEffect(() => {
+    let handler = (e) => {
+      if (!chartMenu.current.contains(e.target)) {
+        setIsActive(false);
+      }
     };
 
-    const clickTable = () => {
-        props.getTypeChart('table')
-        setIsActive(!isActive);
-    }
+    document.addEventListener("mousedown", handler);
 
-    const clickColumn = () => {
-        props.getTypeChart('column');
-        setIsActive(!isActive);
-     }
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
-    const clickPie = () => {
-       props.getTypeChart('pie');
-       setIsActive(!isActive);
-    }
-    const clickBar = () => {
-        props.getTypeChart('bar');
-        setIsActive(!isActive);
-     }
-     const clickStack = () => {
-        props.getTypeChart('stack');
-        setIsActive(!isActive);
+  const ToggleClass = () => {
+    setIsActive(!isActive);
+  };
 
-     }
-     const clickLine = () => {
-        props.getTypeChart('line');
-        setIsActive(!isActive);
-     }
-     const clickScatter = () => {
-        props.getTypeChart('scatter');
-        setIsActive(!isActive);
-     }
+  const clickTable = () => {
+    props.getTypeChart("table");
+    setIsActive(!isActive);
+  };
 
-     const clickArea = () => {
-        props.getTypeChart('area');
-        setIsActive(!isActive);
-     }
+  const clickColumn = () => {
+    props.getTypeChart("column");
+    setIsActive(!isActive);
+  };
 
-     const clickDonut = () => {
-        props.getTypeChart('donut');
-        setIsActive(!isActive);
-     }
+  const clickPie = () => {
+    props.getTypeChart("pie");
+    setIsActive(!isActive);
+  };
+  const clickBar = () => {
+    props.getTypeChart("bar");
+    setIsActive(!isActive);
+  };
+  const clickStack = () => {
+    props.getTypeChart("stack");
+    setIsActive(!isActive);
+  };
+  const clickLine = () => {
+    props.getTypeChart("line");
+    setIsActive(!isActive);
+  };
+  const clickScatter = () => {
+    props.getTypeChart("scatter");
+    setIsActive(!isActive);
+  };
+
+  const clickArea = () => {
+    props.getTypeChart("area");
+    setIsActive(!isActive);
+  };
+
+  const clickDonut = () => {
+    props.getTypeChart("donut");
+    setIsActive(!isActive);
+  };
   return (
     <>
-        <div className='chartItems_container'>
-            {/*======= Chart table ========= */}
-            <div className='chart_table'>
-                <button 
-                    type="button" 
-                    class="btn btn-primary" 
-                    id="liveToastBtn"
-                    onClick={clickTable}
-                >
-                    <BiTable/>
-                    <p>Table</p>
-                </button>
-            </div>
-            {/*======= Chart ========= */}
-            <div className='charts' ref={chartMenu}>
-                <button 
-                    type="button" 
-                    className="btn btn-primary" 
-                    id="liveToastBtn"
-                    onClick={ToggleClass}
-                >
-                    <BsBarChart/>
-                    <p>Chart</p>
-                </button>
-                <div  className={isActive ? "hidden" : 'active'}>
-                    <div className='items_container'>
-                        <div className='item' onClick={clickColumn}>
-                            <BsBarChart/>
-                            <span>Column Chart</span>
-                        </div>
-                        <div className='item'>
-                            <RiBarChartGroupedFill/>
-                            <span>Grouped Col</span>
-                        </div>
-                        <div className='item' onClick={clickStack}>
-                            <MdOutlineStackedBarChart/>
-                            <span>Stacked Col</span>
-                        </div>
-                        <div className='item' onClick={clickBar}>
-                            <RiBarChartHorizontalFill/>
-                            <span >Bar Chart</span>
-                        </div>
-                        <div className='item'>
-                            <RiBarChartHorizontalLine/>
-                            <span>Grouped Chart</span>
-                        </div>
-                        <div className='item'>
-                            <RiBarChartHorizontalLine/>
-                            <span>Stacked Chart</span>
-                        </div>
-                        <div className='item' onClick={clickScatter}>
-                            <AiOutlineDotChart/>
-                            <span>Scatter</span>
-                        </div>
-                        <div className='item' onClick={clickPie}>
-                            <FcPieChart/>
-                            <span>Pie Chart</span>
-                        </div>
-                        <div className='item' onClick={clickLine}>
-                            <FcLineChart/>
-                            <span>Line Chart</span>
-                        </div>
-                        <div className='item' onClick={clickArea}>
-                            <FcAreaChart/>
-                            <span>Area Chart</span>
-                        </div>
-                        <div className='item' onClick={clickDonut}>
-                            <FcDoughnutChart/>
-                            <span>Donut Chart</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div className="chartItems_container">
+        {/*======= Chart table ========= */}
+        <div className="chart_table">
+          <button
+            type="button"
+            className="btn btn-primary"
+            id="liveToastBtn"
+            onClick={clickTable}
+          >
+            <BiTable />
+            <p>Table</p>
+          </button>
         </div>
+        {/*======= Chart ========= */}
+        <div className="charts" ref={chartMenu}>
+          <button
+            type="button"
+            className="btn btn-primary"
+            id="liveToastBtn"
+            onClick={ToggleClass}
+          >
+            <BsBarChart />
+            <p>Chart</p>
+          </button>
+          <div className={isActive ? "hidden" : "active"}>
+            <div className="items_container">
+              <div className="item" onClick={clickColumn}>
+                <BsBarChart />
+                <span>Column Chart</span>
+              </div>
+              <div className="item">
+                <RiBarChartGroupedFill />
+                <span>Grouped Col</span>
+              </div>
+              <div className="item" onClick={clickStack}>
+                <MdOutlineStackedBarChart />
+                <span>Stacked Col</span>
+              </div>
+              <div className="item" onClick={clickBar}>
+                <RiBarChartHorizontalFill />
+                <span>Bar Chart</span>
+              </div>
+              <div className="item">
+                <RiBarChartHorizontalLine />
+                <span>Grouped Chart</span>
+              </div>
+              <div className="item">
+                <RiBarChartHorizontalLine />
+                <span>Stacked Chart</span>
+              </div>
+              <div className="item" onClick={clickScatter}>
+                <AiOutlineDotChart />
+                <span>Scatter</span>
+              </div>
+              <div className="item" onClick={clickPie}>
+                <FcPieChart />
+                <span>Pie Chart</span>
+              </div>
+              <div className="item" onClick={clickLine}>
+                <FcLineChart />
+                <span>Line Chart</span>
+              </div>
+              <div className="item" onClick={clickArea}>
+                <FcAreaChart />
+                <span>Area Chart</span>
+              </div>
+              <div className="item" onClick={clickDonut}>
+                <FcDoughnutChart />
+                <span>Donut Chart</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="icon_chartItems">
+          <AiOutlineDoubleLeft onClick={() => props.handleDisplayCompent()} />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ChartItems
+export default ChartItems;
