ad7c2d04a5a49cd722fe8274220a080484481539
trantuyen082001@gmail.com <trantuyen082001@gmail.com>
2022-07-28 09:20:04 +0700

    display and hidden when click button

--- react-app/src/components/Charts/AreaChart.js
+++ react-app/src/components/Charts/AreaChart.js
@@ -1,128 +1,168 @@
-import React from 'react'
-import { VictoryChart, VictoryGroup, VictoryAxis, VictoryTheme, VictoryLegend, VictoryTooltip, VictoryArea } from 'victory';
-import { sum, max, min, average, countColumn, getLegend } from "../../hook/index"
+import React from "react";
+import {
+  VictoryChart,
+  VictoryGroup,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryLegend,
+  VictoryTooltip,
+  VictoryArea,
+} from "victory";
+import {
+  sum,
+  max,
+  min,
+  average,
+  countColumn,
+  getLegend,
+} from "../../hook/index";
 import calculateTextWidth from "calculate-text-width";
-import './chartItem.css';
-import { TiDeleteOutline } from 'react-icons/ti';
+import "./chartItem.css";
+import { TiDeleteOutline } from "react-icons/ti";
 
 const AreaChart = (props) => {
-    const { xAxis, yAxis, data, legend } = props;
-    const colors = ["gold", "blue", "green", "yellow", "black", "grey", "darkgreen", "pink", "brown", "slateblue", "grey1", "orange"]
-    // config 
-    const width = 800, height = 500;
-    const style = {
-        area: { data: { stroke: "#c43a31", strokeWidth: 1, strokeLinecap: "round", fillOpacity: 0.5, fill: '#1D81A2' }},
-        xAxis: {
-            axis: { stroke: "#756f6a" },
-            axisLabel: { fontSize: 14, padding: 35 },
-            // ticks: { stroke: "grey", size: 1},
-            tickLabels: { fontSize: 10, padding: 10, },  // angle: 45
+  const { xAxis, yAxis, data, legend } = props;
+  const colors = [
+    "gold",
+    "blue",
+    "green",
+    "yellow",
+    "black",
+    "grey",
+    "darkgreen",
+    "pink",
+    "brown",
+    "slateblue",
+    "grey1",
+    "orange",
+  ];
+  // config
+  const width = 800,
+    height = 500;
+  const style = {
+    area: {
+      data: {
+        stroke: "#c43a31",
+        strokeWidth: 1,
+        strokeLinecap: "round",
+        fillOpacity: 0.5,
+        fill: "#1D81A2",
+      },
+    },
+    xAxis: {
+      axis: { stroke: "#756f6a" },
+      axisLabel: { fontSize: 14, padding: 35 },
+      // ticks: { stroke: "grey", size: 1},
+      tickLabels: { fontSize: 10, padding: 10 }, // angle: 45
+    },
+    yAxis: {
+      axis: { stroke: "#756f6a" },
+      axisLabel: { fontSize: 14, alignItems: "left", padding: 35 },
+      tickLabels: { fontSize: 10, padding: 5 },
+      ticks: {
+        size: () => {
+          const tickSize = 10;
+          return tickSize;
         },
-        yAxis: {
-            axis: { stroke: "#756f6a" },
-            axisLabel: { fontSize: 14, alignItems: 'left', padding: 35 },
-            tickLabels: { fontSize: 10, padding: 5 },
-            ticks: {
-                size: () => {
-                    const tickSize = 10;
-                    return tickSize;
-                },
-                stroke: "black",
-                strokeWidth: 1
-            },
-        },
-        labelXAxis: { fontSize: width / 50 },
-        labelTick: { fontSize: width / 100 },
-        labelYAxis: { fontSize: width / 50, alignItems: 'left', padding: 35 },
-        legend: { border: { stroke: "black" }, title: { fontSize: 14 }, labels: { fontSize: 7 } },
-        tooltip: {
-            stroke: 'grey',
-            fill: 'white',
-            shadowColor: 'grey',
-            shadowWidth: 5
-        }
-    }
+        stroke: "black",
+        strokeWidth: 1,
+      },
+    },
+    labelXAxis: { fontSize: width / 50 },
+    labelTick: { fontSize: width / 100 },
+    labelYAxis: { fontSize: width / 50, alignItems: "left", padding: 35 },
+    legend: {
+      border: { stroke: "black" },
+      title: { fontSize: 14 },
+      labels: { fontSize: 7 },
+    },
+    tooltip: {
+      stroke: "grey",
+      fill: "white",
+      shadowColor: "grey",
+      shadowWidth: 5,
+    },
+  };
 
-    const handleDelete = () => {
-        
-    }
+  return (
+    <>
+      <div className="chartContainer_Items">
+        <VictoryChart
+          responsive={false}
+          theme={VictoryTheme.material}
+          // domainPadding={{ x: [paddingX, paddingX], y: [50, 50] }}
+          height={height}
+          width={width}
+          //</>theme={VictoryTheme.material}
+        >
+          <VictoryGroup>
+            {!legend ? (
+              yAxis.map((y, index) => {
+                return (
+                  <VictoryArea
+                    data={max(data, xAxis, y)}
+                    x={(d) => d[xAxis]}
+                    y={(d) => d[y]}
+                    style={{
+                      data: {
+                        stroke: "#c43a31",
+                        strokeWidth: 1,
+                        strokeLinecap: "round",
+                        fillOpacity: 0.4,
+                        fill: `${colors[index]}`,
+                      },
+                    }}
+                  ></VictoryArea>
+                );
+              })
+            ) : (
+              <VictoryArea
+                data={max(data, xAxis, yAxis[0])}
+                x={(d) => d[xAxis]}
+                y={(d) => d[yAxis[0]]}
+                style={style.area}
+              ></VictoryArea>
+            )}
+          </VictoryGroup>
+          <VictoryAxis
+            crossAxis
+            label={props.xAxis}
+            tickFormat={(t) => {
+              if (!isNaN(t)) return t;
+              const words = t.split(" ");
 
-    return (
-        <>
-            <div className='chartContainer_Items'>
-                <VictoryChart
-                    responsive={false}
-                    theme={VictoryTheme.material}   
-                    // domainPadding={{ x: [paddingX, paddingX], y: [50, 50] }}
-                    height={height}
-                    width={width}
-                //</>theme={VictoryTheme.material}
-                >
-                   
-                    <VictoryGroup>
-                        {(!legend ? yAxis.map((y,index) => {
-                            
-                            return (<VictoryArea
-                                data={max(data, xAxis, y)}
-                                x = {d => d[xAxis]}
-                                y = {d => d[y]}
-                                style={{ data: { stroke: "#c43a31", strokeWidth: 1, strokeLinecap: "round", fillOpacity: 0.4, fill: `${colors[index]}` }}}>
-        
-                            </VictoryArea>)
-                        })
-                        :
-                        <VictoryArea
-                            data={max(data, xAxis, yAxis[0])}
-                            x = {d => d[xAxis]}
-                            y = {d => d[yAxis[0]]}
-                            style={style.area}>
+              let result = "";
+              let row = "";
+              for (let word of words) {
+                row += `${word} `;
+                const calculatedWidth = calculateTextWidth(
+                  row,
+                  `normal ${style.labelTick.fontSize}px sans-serif`
+                );
+                if (calculatedWidth > 21) {
+                  result += `\n${word}`;
+                  row = `${word} `;
+                } else result += ` ${word}`;
+              }
+              return result;
+            }}
+            style={style.xAxis}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(t) => {
+              if (t % 1000000 === 0) return `${t / 1000000}M`;
 
-                        </VictoryArea>
-                        )}
-                        
-                    </VictoryGroup>
-                    <TiDeleteOutline className='delete_icon' onClick={() => handleDelete()}/>
-                    <VictoryAxis
-                        crossAxis
-                        label={props.xAxis}
-                    
-                        tickFormat={(t) => {
-                            if (!isNaN(t)) return t;
-                            const words = t.split(' ');
-
-                            let result = '';
-                            let row = '';
-                            for (let word of words) {
-                                row += `${word} `;
-                                const calculatedWidth = calculateTextWidth(row, `normal ${style.labelTick.fontSize}px sans-serif`)
-                                if (calculatedWidth > 21) {
-                                    result += `\n${word}`;
-                                    row = `${word} `;
-                                }
-                                else result += ` ${word}`;
-                            }
-                            return result;
-                        }}
-                        style={style.xAxis}
-                    />
-                    <VictoryAxis
-                        dependentAxis
-                        tickFormat={(t) => {
-                            if (t % 1000000 === 0)
-                                return `${t / 1000000}M`;
-
-                            if (t % 1000 === 0)
-                                return `${t / 1000}K`;
-                            return t;
-                        }}
-                        label={(yAxis.length == 1 || legend) ? yAxis[0] : 'Frequency'}
-                        style={style.yAxis}
-                    />
-                </VictoryChart>
-            </div>
-        </>
-    );
-
-}
+              if (t % 1000 === 0) return `${t / 1000}K`;
+              return t;
+            }}
+            label={yAxis.length == 1 || legend ? yAxis[0] : "Frequency"}
+            style={style.yAxis}
+          />
+        </VictoryChart>
+      </div>
+    </>
+  );
+};
 
 export default AreaChart;
--- react-app/src/components/Charts/Wrapper.js
+++ react-app/src/components/Charts/Wrapper.js
@@ -1,111 +1,141 @@
-import React from 'react';
-import { useState,  createContext} from 'react';
+import React from "react";
+import { useState, createContext } from "react";
 import { Rnd } from "react-rnd";
-import AreaChart from './AreaChart';
-import BarChart from './BarChart';
-import ColumnChart from './ColumnChart';
-import PieChart from './PieChart';
-import LineChart from './LineChart';
-import { TiDeleteOutline } from 'react-icons/ti';
+import AreaChart from "./AreaChart";
+import BarChart from "./BarChart";
+import ColumnChart from "./ColumnChart";
+import PieChart from "./PieChart";
+import LineChart from "./LineChart";
+import { TiDeleteOutline } from "react-icons/ti";
 
 const WrapperContext = createContext();
 
 const Wrapper = (props) => {
-    const height = 500, width = 800;
-    const {xAxis, yAxis, legend, smallMultiples, values, sizes, secondaryY_Axis} = props.fields;
-    const {index, type, fields, listChart} = props;
+  const height = 500,
+    width = 800;
+  const {
+    xAxis,
+    yAxis,
+    legend,
+    smallMultiples,
+    values,
+    sizes,
+    secondaryY_Axis,
+  } = props?.fields;
+  const { index, type, fields, listChart } = props;
 
+  // console.log('listChart', props.listChart)
 
-    // console.log('listChart', props.listChart)
+  const clickWrapperHandler = (e) => {
+    props.onClickWrapper(props.index, props.type);
+  };
 
-    const clickWrapperHandler = (e) => {
-        props.onClickWrapper(props.index, props.type );
+  // -- handle delete Chart --
+  const handleDeleteChart = (type, index) => {
+    props.handleDeleteChart(type, index);
+  };
+
+  // -- create Chart --
+  const createChart = () => {
+    try {
+      switch (type) {
+        case "column":
+          return xAxis && yAxis ? (
+            <ColumnChart
+              data={props.data}
+              smallMultiple={smallMultiples ? smallMultiples[0] : null}
+              yAxis={yAxis}
+              xAxis={xAxis[0]}
+              legend={legend ? legend[0] : null}
+            />
+          ) : null;
+        case "bar":
+          return xAxis && yAxis ? (
+            <BarChart
+              data={props.data}
+              yAxis={yAxis}
+              xAxis={xAxis[0]}
+              legend={legend ? legend[0] : null}
+            />
+          ) : null;
+        case "pie":
+          return values && legend ? (
+            <PieChart data={props.data} value={values} legend={legend} />
+          ) : null;
+        // case 'donut':
+        //     return <DonutChart/>
+        case "line":
+          return xAxis && yAxis ? (
+            <LineChart
+              yAxis={yAxis}
+              xAxis={xAxis[0]}
+              data={props?.data}
+              legend={legend ? legend[0] : null}
+            />
+          ) : null;
+        case "area":
+          return xAxis && yAxis ? (
+            <AreaChart data={props.data} yAxis={yAxis} xAxis={xAxis[0]} />
+          ) : null;
+        default:
+          return null;
+      }
+    } catch (e) {
+      console.log(e);
+      return null;
     }
+  };
 
-    // -- handle delete Chart --
-    const handleDeleteChart = (type, index) => {
-        props.handleDeleteChart(type,index);
-    }
-
-    // -- create Chart --
-    const createChart = () => {
-        try {
-            switch(type){
-                case 'column':
-                    return (xAxis && yAxis  ?
-                        <ColumnChart 
-                            data={props.data} 
-                            smallMultiple={(smallMultiples) ? smallMultiples[0] : null} 
-                            yAxis={yAxis} xAxis={xAxis[0]} 
-                            legend={legend ? legend[0] : null}/>:null)
-                case 'bar':
-                    return (xAxis && yAxis ? <BarChart data={props.data} yAxis={yAxis} xAxis={xAxis[0]} legend={legend ? legend[0] : null}/> : null)
-                case 'pie':
-                    return (values && legend ? <PieChart data={props.data} value={values} legend={legend}/> : null)
-                // case 'donut':
-                //     return <DonutChart/>
-                case 'line':
-                    return (xAxis && yAxis ? 
-                    <LineChart yAxis={yAxis} 
-                            xAxis={xAxis[0]}
-                            data={props.data}
-                            legend={legend ? legend[0] : null}
-                        /> : null)
-                case 'area':
-                    return (xAxis && yAxis ? <AreaChart data={props.data} yAxis={yAxis} xAxis={xAxis[0]} /> : null)
-                default: return null;
-            }
-        }
-        catch(e) {
-            console.log(e)
-            return null;
-        }
-    }
-
-    const style = {
-        rnd: {
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            border: "solid 2px #1D81A2",
-            borderStyle: "dotted",
-            position: 'relative'
-        },
-        legend: { title: { fontSize: 20 }, labels: { fontSize: 12 },
-        tooltip: {
-            stroke: 'grey',
-            fill: 'white',
-            shadowColor: 'grey',
-            shadowWidth: 5
-        }},
-        icon: {
-            position: 'absolute',
-            display: 'block',
-            right: '5px',
-            top: '5px',
-            fontSize: '20px',
-            cursor: 'pointer',
-            height: '20px',
-            zIndex: '1000'
-        }        
-    };
-    return (
-            <Rnd
-                onClick={clickWrapperHandler}
-                style={style.rnd}
-                default={{
-                    x: 0,
-                    y: 0,
-                    width: 400,
-                    height: 300
-                }}
-                bounds={"parent"}
-                key={index}
-            >
-                <TiDeleteOutline style={style.icon} onClick={() => handleDeleteChart(type,index)} />
-                {createChart()}
-            </Rnd>
-    );
-}
+  const style = {
+    rnd: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      border: "solid 2px #1D81A2",
+      borderStyle: "dotted",
+      position: "relative",
+    },
+    legend: {
+      title: { fontSize: 20 },
+      labels: { fontSize: 12 },
+      tooltip: {
+        stroke: "grey",
+        fill: "white",
+        shadowColor: "grey",
+        shadowWidth: 5,
+      },
+    },
+    icon: {
+      position: "absolute",
+      display: "block",
+      right: "5px",
+      top: "5px",
+      fontSize: "20px",
+      cursor: "pointer",
+      height: "20px",
+      zIndex: "1000",
+    },
+  };
+  return (
+    <Rnd
+      onClick={clickWrapperHandler}
+      style={style.rnd}
+      default={{
+        x: 0,
+        y: 0,
+        width: 400,
+        height: 300,
+      }}
+      bounds={"parent"}
+      key={index}
+    >
+      <TiDeleteOutline
+        style={style.icon}
+        onClick={() => handleDeleteChart(type, index)}
+      />
+      {createChart()}
+    </Rnd>
+  );
+};
 
 export default Wrapper;
